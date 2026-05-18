dc52215412d9c72e43acc8e3ac47d5e016f4bc4b
maxwellfairman <maxwellafairman@gmail.com>
2026-05-18 14:46:07 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "See you, Space Cowboy...";
+//this changes first found h1 text content to "See you, Space Cowboy...onst means not changeable
+const myImg = document.querySelector("img");
+myImg.addEventListener("click", () => {
+    const mySrc = myImg.getAttribute("src");
+    if (mySrc === "images/new spike pic.jpg") {
+        myImg.setAttribute("src", "images/jet-p99.jpg");
+    }else{
+        myImg.setAttribute("src", "images/new spike pic.jpg");
+    }
+});
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName(){
+    const myName = prompt("Please enter your name");
+    localStorage.setItem("name", myName); //local storage is for key value pairs, stores in browser, can use for user preferences
+    myHeading.textContent = `See you, ${myName}...`;
+}
+if(!localStorage.getItem("name")){
+    //null = false
+    setUserName();
+}else{
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `See you, ${storedName}...`;
+}
+//use ` for formatted strings, ${} for variables in string
+//const and let are block scoped, avoid var
+//const for unchanging variables, let for changing variables
